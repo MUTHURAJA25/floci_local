3e566d24857737ed13dd736c5a8b5fc4e93f47db
MUTHURAJA25 <muthucloud25@gmail.com>
2026-06-18 19:38:26 +0530

    DynamoDb_Remove

--- main.tf
+++ main.tf
@@ -30,17 +30,4 @@ provider "aws" {
 
 }
 
-resource "aws_dynamodb_table" "lock" {
-
-  name = "tf-lock"
-
-  billing_mode = "PAY_PER_REQUEST"
-
-  hash_key = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
 
